8f656ce5853e5b4b8d65ebc6eb35822ab6da7c12
Hagou <litimhaq@hotmail.fr>
2023-03-08 17:08:34 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '8803d935-c5fa-4e80-a9d7-8794b32f7520';
+export const MODAL_CLOSE_SEC = 2.5;
